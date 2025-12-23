e74c3804d7574027f8aecb8ce17b9c05114f6b47
yodogawa404 <shiontak2oo5@gmail.com>
2025-12-24 05:11:57 +0900

    WIP

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ npm i @yodogawa404/get-unicode-chunk-range_jp
 ```
 
 ```js
-import unicode_ranges from "@yodogawa404/get-unicode-chunk-range_jp/jp.json" with {type:'json'}
+import unicode_ranges from "@yodogawa404/get-unicode-chunk-range_jp/jp.json" with { type: "json" };
 
-console.log(unicode_ranges)
+console.log(unicode_ranges);
 ```
--- scripts/generator.js
+++ scripts/generator.js
@@ -1,31 +1,33 @@
-import fs from 'fs';
+import fs from "fs";
 const __dirname = import.meta.dirname;
 
-let fetcher =await fetch("https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap", {
-  "headers": {
-    "User-Agent":"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116"
+let fetcher = await fetch(
+  "https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap",
+  {
+    headers: {
+      "User-Agent":
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116",
+    },
+    body: null,
+    method: "GET",
   },
-  "body": null,
-  "method": "GET"
-});
+);
 
+let text = await fetcher.text();
 
-let text = await fetcher.text()
+const match = text.matchAll(/unicode-range:(.*;)$/gm);
+const arr = Array.from(match, (m) => m[1]);
 
-const match = text.matchAll(/unicode-range:(.*;)$/mg)
-const arr = Array.from(match,m=>m[1])
+let newarr = [];
 
-let newarr=[]
+for (let i = 0; i < arr.length; i++) {
+  let inner_arr = arr[i].replaceAll(";", "").split(", ");
 
-for(let i=0;i<arr.length;i++){
-    let inner_arr = arr[i].replaceAll(";","").split(", ")
+  for (let ii = 0; ii < inner_arr.length; ii++) {
+    inner_arr[ii] = inner_arr[ii].replaceAll(" ", "");
+  }
 
-    for(let ii=0;ii<inner_arr.length;ii++){
-        inner_arr[ii]=inner_arr[ii].replaceAll(" ","");
-    }
-    
-    newarr.push(inner_arr)
-    
+  newarr.push(inner_arr);
 }
 
-fs.writeFileSync(__dirname+'/../jp.json',JSON.stringify(newarr))
+fs.writeFileSync(__dirname + "/../jp.json", JSON.stringify(newarr));
